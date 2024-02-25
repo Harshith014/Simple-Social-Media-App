3711e26d70f9283838b506e476e92ffcc514b777
Harshith <harshith2742@gmail.com>
2024-02-25 22:22:22 +0530

    Update files and add backend directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import PostFeed from './components/PostFeed';
+import Home from './components/Home';
+import CreatePosts from './components/CreatePosts';
+// import UserList from './components/UserList';
 import './App.css';
+// import UpdateProfile from './components/UpdateProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="app" style={{overflow:'hidden'}}>
+				<nav >
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/postfeed">Post Feed</Link>
+						</li>
+						<li>
+							<Link to="/create">Create Post</Link>
+						</li>
+						{/* <li>
+							<Link to="/profile">User Update Profile</Link>
+						</li> */}
+					</ul>
+				</nav>
+				{/* <UserList/> */}
+				<Routes>
+					<Route path="/create" element={<CreatePosts />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/postfeed" element={<PostFeed />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					{/* <Route path="/profile" element={<UpdateProfile />} /> */}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
